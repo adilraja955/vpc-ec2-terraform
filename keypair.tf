093bf6e82bb480c0b32939ff22b9352cd8840aca
adilraja955 <maadil2020@outlook.com>
2026-02-12 00:32:38 +0500

    changes to keypair file

--- keypair.tf
+++ keypair.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "bastion" {
   key_name   = "terraform-bastion"
-  public_key = file("~/.ssh/terraform-bastion.pub")
+  public_key = file("terraform-bastion.pub")
 
   tags = {
     Name = "terraform-bastion-key"
